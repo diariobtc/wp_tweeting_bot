4746a364faa590763e3bebe1c876768accd16c17
Angel Leon <gubatron@gmail.com>
2021-02-04 10:32:29 -0500

    new common.sh

---
+++ common.sh
@@ -0,0 +1,10 @@
+abort_if_var_unset() {
+  # Usage: abort_if_var_unset "OPENSSL_VERSION" ${OPENSSL_VERSION}
+  var_name=${1}
+  var_value=${2}
+  if [[ ! -n ${var_value} ]]; then
+    prompt_msg "Error ${var_name} not set. Aborting"
+    exit 1
+  fi
+  echo ${var_name} set to ${var_value}
+}
